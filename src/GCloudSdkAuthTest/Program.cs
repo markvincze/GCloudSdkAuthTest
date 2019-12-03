0ea11a2c75a8cd84c7fb138e2ccbfa788d38cecb
Mark Vincze <mrk.vincze@gmail.com>
2019-12-03 16:31:26 +0100

    Initial commit

---
+++ src/GCloudSdkAuthTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Google.Cloud.Datastore.V1;
+using Newtonsoft.Json;
+
+namespace GCloudSdkAuthTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var db = DatastoreDb.Create("dev-test-travix-com", "travix-enosis");
+
+                var key = db.CreateKeyFactory("ItineraryCache").CreateKey("20170911164022999-Stubbed-2016-03-22");
+                var result = db.Lookup(key);
+
+                Console.WriteLine("Entity was retrieved: {0}", JsonConvert.SerializeObject(result));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
